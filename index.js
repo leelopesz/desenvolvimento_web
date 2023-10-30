473c961e9ce4b275a39e783e14c05720fcfd9174
EA - Leticia Bezerra Lopes <bezerra_lopes@educadventista.org>
2023-10-30 15:09:03 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+    console.log(`Acessando dados do usuário ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
     console.log(caminhoBase)
